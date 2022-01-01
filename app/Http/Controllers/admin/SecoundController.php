39696bff524e3c8b2091eb35b1b4c3bdee929a1f
Your Name <you@example.com>
2022-01-01 21:45:31 +0200

    first commit

---
+++ app/Http/Controllers/admin/SecoundController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class SecoundController extends Controller
+{
+    public function show_string(){
+        return "controller lesson";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Auth::routes(['verify'=>true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
